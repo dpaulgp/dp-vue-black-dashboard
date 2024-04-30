de30ea096eba616dd2a506e5782f84baa75aebdb
dpaulgp <93206701+dpaulgp@users.noreply.github.com>
2024-04-30 17:27:13 +0200

    boton edicion turnos

--- src/pages/TableList.vue
+++ src/pages/TableList.vue
@@ -3,16 +3,25 @@
     <div class="col-12">
       <card :title="table1.title">
         <div class="table-responsive">
-          <base-table
-            :data="table1.data"
-            :columns="table1.columns"
-            thead-classes="text-primary"
-          >
+          <base-table :data="table1.data" :columns="table1.columns" thead-classes="text-primary">
+            <template slot-scope="{ row }">
+              <td>{{ row.trx }}</td>
+              <td>{{ row.nombre }}</td>
+              <td>{{ row.usuario }}</td>
+              <td>{{ row.estado }}</td>
+              <td>{{ row.inicio }}</td>
+              <td>{{ row.fin }}</td>
+              <td>{{ row.fecha }}</td>
+              <td class="td-actions text-right">
+                <base-button type="button" @click="editTurno(row)">
+                  <i class="tim-icons icon-pencil"></i>
+                </base-button>
+              </td>
+            </template>
           </base-table>
         </div>
       </card>
     </div>
-
   </div>
 </template>
 <script>
@@ -48,6 +57,9 @@ export default {
           console.error('Error fetching data:', error);
           alert('Error fetching Turnos data'); 
         });
+    },
+    editTurno(row) {
+      alert('Editando turno: ' + row.trx);  
     }
   }
 };
